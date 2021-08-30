ad764aa5c3986e9808ca67cbe4afc2789a1af596
Rende <jerrett.d.rende@accenturefederal.com>
2021-08-30 11:59:22 -0400

    added rudimentary server-side form validation

--- Puckdrop-2/src/main/java/com/spring/mvc/model/User.java
+++ Puckdrop-2/src/main/java/com/spring/mvc/model/User.java
@@ -1,14 +1,20 @@
 package com.spring.mvc.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
 	
-	private String name;
+	@NotNull
+	@Size(min=4, max=20)
 	private String username;
-	private NhlTeam favoriteTeam;
 	
-	public String getName() {
-		return this.name;
-	} 
+	@NotNull
+	@Size(min=8, max=20)
+	private String password;
+	
+	@NotNull
+	private NhlTeam favoriteTeam;
 	
 	public String getUsername() {
 		return this.username;
@@ -18,12 +24,12 @@ public class User {
 		return this.favoriteTeam;
 	}
 	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 	public void setUsername(String username) {
 		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
 	public void setFavoriteTeam(NhlTeam favoriteTeam) {
